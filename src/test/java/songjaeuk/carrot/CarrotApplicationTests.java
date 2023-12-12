4b35db9e9021844fe650d90cf521665613488274
chung2code <anthdny7@gmail.com>
2023-12-12 16:07:43 +0900

    .

--- src/test/java/songjaeuk/carrot/CarrotApplicationTests.java
+++ src/test/java/songjaeuk/carrot/CarrotApplicationTests.java
@@ -3,7 +3,7 @@ package songjaeuk.carrot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CarrotApplicationTests {
 
 	@Test
